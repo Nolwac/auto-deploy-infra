e857e9f0cdad3fa2259bef77559a9f961260c74e
Nolwac <livinusanayo96@gmail.com>
2022-06-24 07:28:33 +0100

    Test create inventory

--- create_inventories.sh
+++ create_inventories.sh
@@ -1,3 +1,2 @@
-touch inventory
 echo "[all]" > inventory
 aws ec2 describe-instances --filters "Name=tag:Project,Values=udacity" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text >> inventory
